90eb55e9bde30922ad3d9695f3c7cc0032beebfc
MagmaMeow <Kannunanu024@gmail.com>
2025-09-10 17:32:12 +0530

    Delete scheduler.py

--- scheduler.py
+++
@@ -1,9 +0,0 @@
-import aioschedule
-import asyncio
-from bot import weekly_mod_check
-
-async def scheduler():
-    aioschedule.every().saturday.at("01:00").do(weekly_mod_check)
-    while True:
-        await aioschedule.run_pending()
-        await asyncio.sleep(60)
